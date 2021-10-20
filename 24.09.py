02be9e87d08288b1a96f0df3ee4ee807df5f385a
MinLiway <89976190+MinLiway@users.noreply.github.com>
2021-10-20 15:41:01 +0300

    24.09

---
+++ 24.09.py
@@ -0,0 +1,46 @@
+
+# print(f.read())
+# f.close()
+
+# f=open('C://Users//min//my_project//text.txt', 'w')
+# print(f.write('GGGGGGGGGGGGGG'))
+# f.close()
+
+# f=open('C://Users//min//my_project//text.txt', 'a')
+# print(f.write("DDDDDDDDDDD"))
+# f.close()
+
+# f=open('C://Users//min//my_project//text.txt', 'r')
+# str_list = f.readlines()
+# print(str_list)
+# f.close()
+
+# f=open('C://Users//min//my_project//text.txt', 'w')
+# f.writelines(['BBBBBBBBBBBBB', 'AAAAAAAAAAAA'])
+# f.close()
+
+# f=open('C://Users//min//my_project//text.txt', 'r')
+
+# f = open('C://Users//min//my_project//text.txt', 'r')
+# lines = f.readlines()
+# lines = [line[:-1] for line in lines]
+# d = {lines[1]:lines[2], lines[3]: int(lines[4])}
+# print(d)
+# f.close()
+
+ip = { }
+f = open('C://Users//min//my_project//text.txt', 'r')
+p = f.readlines()
+l = 0
+for i in range(len(p)):
+    if i % 2 == 0:
+        v = p[i][:-1]
+    else:
+        if i !=len(p):
+            l=p[i]
+        else:
+            l=p[i][:-1]
+        ip[v]=l
+print(ip)
+f.close()
+
