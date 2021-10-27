4c8619065c0ff9900c180442d84dc599dd9d5215
MinLiway <89976190+MinLiway@users.noreply.github.com>
2021-10-27 09:05:19 +0300

    24.09

--- 24.09.py
+++ 24.09.py
@@ -1,42 +1,51 @@
-# f=open('C://Users//min//my_project//text.txt', 'w')
-# print(f.write('GGGGGGGGGGGGGG'))
+# f=open('text.txt', 'r')
+# print(f.read())
 # f.close()
 
-# f=open('C://Users//min//my_project//text.txt', 'a')
-# print(f.write("DDDDDDDDDDD"))
+# f=open('text.txt', 'w')
+# f.write('poka')
 # f.close()
 
-# f=open('C://Users//min//my_project//text.txt', 'r')
-# str_list = f.readlines()
-# print(str_list)
+# f=open('text.txt', 'a')
+# f.write('privet')
 # f.close()
 
-# f=open('C://Users//min//my_project//text.txt', 'w')
+# f=open('text.txt', 'w')
 # f.writelines(['BBBBBBBBBBBBB', 'AAAAAAAAAAAA'])
 # f.close()
 
-# f=open('C://Users//min//my_project//text.txt', 'r')
-
-# f = open('C://Users//min//my_project//text.txt', 'r')
-# lines = f.readlines()
-# lines = [line[:-1] for line in lines]
-# d = {lines[1]:lines[2], lines[3]: int(lines[4])}
-# print(d)
+# f = open('text.txt')
+# print(f.readlines())
 # f.close()
 
-ip = { }
-f = open('C://Users//min//my_project//text.txt', 'r')
-p = f.readlines()
-l = 0
-for i in range(len(p)):
-    if i % 2 == 0:
-        v = p[i][:-1]
-    else:
-        if i !=len(p):
-            l=p[i]
-        else:
-            l=p[i][:-1]
-        ip[v]=l
-print(ip)
-f.close()
+# f = open('file.txt', 'r')
+# print(f.readlines())
+# f.close
+# new_dict = {f.readlines()}
+# print(new_dict)
 
+
+# f = open('2409.txt', 'r')
+# print(f.read())
+# f.close()
+
+# f = open('2409.txt', 'w')
+# f.write('formula1')
+# print(f.write)
+# f.close()
+
+# f = open('2409.txt', 'r')
+# print(f.readlines())
+# f.close()
+
+# f = open('2409.txt', 'a')
+# f.writelines(['good', 'day'])
+# f.close()
+
+f = open('file.txt', 'r')
+lines = f.readlines()
+lines = [line[:-1] for line in lines]
+dict_ = {'ip': lines[2][:-1], 'port': int(lines[4][:-1])}
+d = {lines[1]:lines[2], lines[3]: int(lines[4])}
+print(d)
+f.close()
